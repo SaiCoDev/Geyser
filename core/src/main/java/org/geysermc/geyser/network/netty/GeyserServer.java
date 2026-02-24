635215b38f02f5446e3569fae30180e064bc40ef
Adam Walker <adam@perk.gg>
2026-02-24 13:40:15 +0000

    Add support for Waterdog proxy IP forwarding

--- core/src/main/java/org/geysermc/geyser/network/netty/GeyserServer.java
+++ core/src/main/java/org/geysermc/geyser/network/netty/GeyserServer.java
@@ -134,7 +134,7 @@ public final class GeyserServer {
             this.listenCount = 1;
         }
 
-        if (this.geyser.config().advanced().bedrock().useHaproxyProtocol()) {
+        if (this.geyser.config().advanced().bedrock().useHaproxyProtocol() || this.geyser.config().advanced().bedrock().useWaterdogExtras()) {
             this.proxiedAddresses = ExpiringMap.builder()
                     .expiration(30 + 1, TimeUnit.MINUTES)
                     .expirationPolicy(ExpirationPolicy.ACCESSED).build();
@@ -260,7 +260,7 @@ public final class GeyserServer {
 
         String ip;
         if (geyser.config().logPlayerIpAddresses()) {
-            if (geyser.config().advanced().bedrock().useHaproxyProtocol()) {
+            if (this.proxiedAddresses != null) {
                 ip = this.proxiedAddresses.getOrDefault(inetSocketAddress, inetSocketAddress).toString();
             } else {
                 ip = inetSocketAddress.toString();
@@ -289,7 +289,7 @@ public final class GeyserServer {
         if (geyser.config().debugMode() && PRINT_DEBUG_PINGS) {
             String ip;
             if (geyser.config().logPlayerIpAddresses()) {
-                if (geyser.config().advanced().bedrock().useHaproxyProtocol()) {
+                if (this.proxiedAddresses != null) {
                     ip = this.proxiedAddresses.getOrDefault(inetSocketAddress, inetSocketAddress).toString();
                 } else {
                     ip = inetSocketAddress.toString();
